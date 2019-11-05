3097385cc13a49bbd6bb24821b1e1637d762ebbc
keninmuchui <keninmuchui@gmail.com>
2019-11-05 16:16:53 +0300

    section 3 commit

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,13 @@ $(".click0").click(function(){
     $(".hide0").slideToggle(1000);
     $(".show0").slideToggle(1200);
   });
+
+  $(".click1").click(function(){
+    $(".hide1").slideToggle(1000);
+    $(".show1").slideToggle(1200);
+  });
+
+  $(".click2").click(function(){
+    $(".hide2").slideToggle(1000);
+    $(".show2").slideToggle(1200);
+  });
